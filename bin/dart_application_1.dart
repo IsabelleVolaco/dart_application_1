972d387d1b10e7c666a4fbc2cbde93d3a693e20e
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-11-13 14:50:24 -0300

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,14 @@
+
+void exibirInfo ({required String nome, required int idade}){
+  print("Nome: $nome, Idade: $idade");
+}
+
+void exibirInfo2 ({String nome = "Julia", int idade = 22}){
+  print("Nome: $nome, Idade: $idade");
+}
+
+void main() {
+  exibirInfo(nome:"Ana", idade: 30);
+  exibirInfo2();
+  exibirInfo2(nome:"Josué");
+}
